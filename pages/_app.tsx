455e81af680635b90e3e9ae3b04dd92190abe637
mahshid22 <mahshid.pouyafar@gmail.com>
2022-06-10 21:47:28 +0430

    add nprogress to _app for showing progressbar for route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { RouteGuard } from "components/RouteGuard";
+import NProgress from "nprogress";
+import "../public/nprogress.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
   return (
     <RouteGuard>
       <Component {...pageProps} />
