6114c464d452c0e65c483aeade32d2fe5b6d02d4
mahshid22 <mahshid.pouyafar@gmail.com>
2022-06-11 10:15:56 +0430

    add pwa config to project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+});
+
 module.exports = {
   images: {
     domains: ["images.vexels.com"],
